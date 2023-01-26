b0667dda109da9423890d4f7ce65b51e7241d9ac
Mohit Vashistha <mohitvashistha@Mohits-MacBook-Pro.local>
2023-01-26 13:46:54 +0530

    Readded website logo for testing

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -144,10 +144,10 @@ const Footer = ({ t }: any) => {
               </LogoContainer>
             </NavLink>
             <FooterContainer>
-            {/* <SocialLink
+            <SocialLink
                 href="https://mohit.blockant.in/"
                 src="website.svg"
-              /> */}
+              />
               <SocialLink
                 href="https://github.com/blockant/"
                 src="github.svg"
